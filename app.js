39e0f3182190a36ae171b88ecc6894eaced7ea79
Jalpesh Patel <jalpesh@192-168-1-127.tpgi.com.au>
2020-05-09 16:01:50 +1000

    Initalized the start game method

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false
+    },
+    methods: {
+        // Initialize the game
+        startGame: function () {
+            this.gameIsRunning = true;
+            // Reset the player health to zero incase if we click start game button after previously ended gamme
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        }
     }
 });
