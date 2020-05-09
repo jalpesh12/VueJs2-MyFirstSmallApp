e0d911961d5a9db63790d41b36572f3e8cf80465
Jalpesh Patel <jalpesh@192-168-1-127.tpgi.com.au>
2020-05-09 16:31:48 +1000

    Implemented attack method

--- app.js
+++ app.js
@@ -9,9 +9,50 @@ new Vue({
         // Initialize the game
         startGame: function () {
             this.gameIsRunning = true;
-            // Reset the player health to zero incase if we click start game button after previously ended gamme
+            // Reset the player health to zero incase if we click start game button after previously ended gamme,
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            if (this.checkWin()) {
+                return;
+            }
+            damage = this.calculateDamage(3, 10);
+            this.playerHealth -= damage;
+            his.checkWin();
+
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveup: function () {
+
+        },
+        calculateDamage: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min)
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! start new game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! start new game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 });
